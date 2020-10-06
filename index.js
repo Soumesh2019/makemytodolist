396309228facd618a43bafaba5970a9950a063d2
Soumesh <sk0564845@gmail.com>
2020-10-06 15:26:19 +0530

    Added user Details

--- index.js
+++ index.js
@@ -15,8 +15,19 @@ mongoose.connect("mongodb://localhost:27017/toDoDB", {
 });
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: [true, "Please Provide username"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Provide password"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Provide email"],
+  },
+  toDoItem: Array,
 });
 
 const userModel = new mongoose.model("user", userSchema);
@@ -26,7 +37,6 @@ app.get("/", (req, res) => {
 });
 
 // Register The User
-
 app
   .route("/register")
   .get((req, res) => {
@@ -35,25 +45,26 @@ app
   .post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    const email = req.body.email;
 
-    userModel.findOne({ username: username }, (err, foundUser) => {
+    userModel.findOne({ email: email }, (err, foundUser) => {
       if (!foundUser) {
         bcrypt.hash(password, saltRounds, function (err, hash) {
           if (!err) {
             const user = new userModel({
               username: username,
+              email: email,
               password: hash,
             });
-
-            user.save((err) => {
+            user.save((err, result) => {
               if (!err) {
                 isLoggedIn = true;
-                res.redirect("/todo");
+                res.redirect("/user/" + result._id);
               }
             });
           } else {
             console.log(err);
-            res.redirect("/login");
+            res.redirect("/register");
           }
         });
       } else {
@@ -62,18 +73,17 @@ app
     });
   });
 
-//   Login The User
-
+// Login The User
 app
   .route("/login")
   .get((req, res) => {
     res.render("login");
   })
   .post((req, res) => {
-    const username = req.body.username;
+    const email = req.body.email;
     const userpassword = req.body.password;
 
-    userModel.findOne({ username: username }, (err, foundUser) => {
+    userModel.findOne({ email: email }, (err, foundUser) => {
       if (!err) {
         if (foundUser) {
           bcrypt.compare(userpassword, foundUser.password, function (
@@ -82,7 +92,7 @@ app
           ) {
             if (result) {
               isLoggedIn = true;
-              res.redirect("/todo");
+              res.redirect("/user/" + foundUser._id);
             } else if (err) {
               console.log(err);
             } else {
@@ -100,20 +110,47 @@ app
     });
   });
 
-//   Todo Section
-
-app.route("/todo").get((req, res) => {
-  if(isLoggedIn) {
-      res.render("todo")
+// Todo Section
+app.get("/user/:userId", (req, res) => {
+  if (isLoggedIn) {
+    const userId = req.params.userId;
+    userModel.findOne({ _id: userId }, (err, foundUser) => {
+      if (!err) {
+        res.render("todo", {
+          userId: userId,
+          name: foundUser.username,
+          itemsArray: foundUser.toDoItem,
+        });
+      }
+    });
   } else {
-      res.redirect("/login")
+    res.redirect("/login");
   }
 });
+app.post("/submit", (req, res) => {
+  const userId = req.body.userId;
+  const item = req.body.item;
 
-app.post("/logout", (req, res)=> {
-    isLoggedIn = false;
-    res.redirect("/")
-})
+  userModel.updateOne(
+    { _id: userId },
+    { $push: { toDoItem: item } },
+    (err, result) => {
+      if (!err) {
+        res.redirect("/user/" + userId);
+      } else if (err) {
+        console.log(err);
+        res.send(new Error(err));
+      }
+    }
+  );
+});
 
+app.post("/completed", (req, res) => {
+});
+
+app.post("/logout", (req, res) => {
+  isLoggedIn = false;
+  res.redirect("/login");
+});
 
 app.listen(3000, () => console.log("Server is running on 3000"));

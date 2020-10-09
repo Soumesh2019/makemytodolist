1e3069443fe8a8d60a3aab69a64d814d113edf50
Soumesh <sk0564845@gmail.com>
2020-10-09 08:45:12 +0530

    Error Fixing

--- index.js
+++ index.js
@@ -218,4 +218,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => console.log("Server is running on 3000"));
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log("Server is running on "+PORT));
